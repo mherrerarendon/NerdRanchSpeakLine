b7560f6fc69caad05fe13111c805cda93d2b3689
Marco Herrera <marcoherrera@Marcos-Mac-Pro.local>
2016-02-17 17:25:57 -0700

    Initial Commit

---
+++ SpeakLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeakLine
+//
+//  Created by Marco Herrera on 2/17/16.
+//  Copyright (c) 2016 Marco Herrera. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
